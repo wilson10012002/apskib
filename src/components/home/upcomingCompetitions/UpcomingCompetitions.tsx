24b0bda27ddb2f4bcd528794a4d7e6dc6678bd93
Wilson Garrido <wilson@42luanda.com>
2026-08-18 12:26:09 +0100

    seccond

--- src/components/home/upcomingCompetitions/UpcomingCompetitions.tsx
+++ src/components/home/upcomingCompetitions/UpcomingCompetitions.tsx
@@ -1,8 +1,8 @@
-const competitions = [
+/*const competitions = [
   {
     title: 'I Campeonato Provincial de Sambo e Kurash',
     date: '12–13 Setembro 2026',
     location: 'Icolo e Bengo',
     category: 'Campeonato Provincial',
   },
-]
+]*/
