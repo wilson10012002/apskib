97038d4dc24a457869c7d2e30aa935d090de6e60
Wilson Garrido <wilson@42luanda.com>
2026-08-18 13:07:38 +0100

    seccond

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from "../../../assets/images/logo.jpeg"
+
 
 export default function Hero() {
   return (
--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from "../../../assets/images/logo.png"
+
 
 export default function Footer() {
   return (
